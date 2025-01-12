af3268f2bc1224731b050d5466864b0f96b4bda4
NicolausFeldtmann <n.feldtmann@yahoo.de>
2025-01-12 21:17:18 +0100

    Hopsa!

---
+++ main.js
@@ -0,0 +1,16 @@
+const BASE_URL = "https://join-marcel-lukas-default-rtdb.europe-west1.firebasedatabase.app/";
+
+
+async function includeHTML() {
+  let includeElements = document.querySelectorAll('[w3-include-html]');
+  for (let i = 0; i < includeElements.length; i++) {
+      const element = includeElements[i];
+      let file = element.getAttribute("w3-include-html");
+      let resp = await fetch(file);
+      if (resp.ok) {
+          element.innerHTML = await resp.text();
+      } else {
+          element.innerHTML = 'Page not found';
+      }
+  }
+}
